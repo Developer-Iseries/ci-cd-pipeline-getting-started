da195c1e2c7f5eb15811f8a515a1a58170232318
Gaurav <gaurav.pradeep@gmail.com>
2026-09-06 11:22:28 +0530

    updated test case in test_main for checing failure scenario

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from src.main import add
 def test_add_function():
     assert add(3, 5) == 8
     assert add(3, 3) == 6
-    assert add(3, 2) == 5
+    assert add(3, 2) == 4
     assert add(3, 1) == 4
     assert add(3, 7) == 10
     assert add(2, 2) == 4
